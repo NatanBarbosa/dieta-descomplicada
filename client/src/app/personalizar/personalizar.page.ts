4a06fe064b59eb55501c3b3df29c4819edde6e5d
Natan Barbosa <natanbarbosa525@gmail.com>
2022-09-21 14:51:07 -0300

    Botão de adicionar em cada modal de personalização de dieta feito. Ainda falta arrumar o modal de alimentos em montar refeição

--- client/src/app/personalizar/personalizar.page.ts
+++ client/src/app/personalizar/personalizar.page.ts
@@ -10,7 +10,11 @@ export class PersonalizarPage implements OnInit {
   public isHorariosOpen = false;
   public isAlimentosOpen = false;
   public isRefeicaoOpen = false;
+  public isAddHorarioOpen = false;
+  public isAddAlimentoOpen = false;
+  public isAddRefeicaoOpen = false;
 
+  //Abrir e fechar modais
   setHorariosOpen(isOpen: boolean) {
     this.isHorariosOpen = isOpen;
   }
@@ -21,6 +25,18 @@ export class PersonalizarPage implements OnInit {
 
   setRefeicaoOpen(isOpen: boolean) {
     this.isRefeicaoOpen = isOpen;
+  }
+
+  setAddHorarioOpen(isOpen: boolean) {
+    this.isAddHorarioOpen = isOpen;
+  }
+
+  setAddAlimentoOpen(isOpen: boolean) {
+    this.isAddAlimentoOpen = isOpen;
+  }
+
+  setAddRefeicaoOpen(isOpen: boolean) {
+    this.isAddRefeicaoOpen = isOpen;
   }
 
   ngOnInit() {
